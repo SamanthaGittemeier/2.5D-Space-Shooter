e8549527be7b31b3eb51af21c443c85b1dff0db4
Samantha Gittemeier <sammig249@gmail.com>
2023-05-19 18:43:51 -0500

    Created Enemy script material and prefab, fixed laser prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(0, -3, 0);
     }
 
     // Update is called once per frame
