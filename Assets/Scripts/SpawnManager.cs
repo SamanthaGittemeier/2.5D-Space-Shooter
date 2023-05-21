1e3a3bf2530733f0c123681c3dc59af99fd169c9
Samantha Gittemeier <sammig249@gmail.com>
2023-05-20 22:05:19 -0500

    Decluttered Enemy Spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,10 +7,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject EnemeyPrefab_SM;
     public float EnemyRandomX;
     public GameObject Player_SM;
+    public GameObject EnemyContainer;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         StartCoroutine(SpawnEnemies());
     }
 
@@ -20,17 +22,13 @@ public class SpawnManager : MonoBehaviour
         
     }
 
-    //void EnemySpawnDelay()
-    //{
-    //    yield return new WaitForSeconds(5);
-    //}
-
     IEnumerator SpawnEnemies()
     {
         while (Player_SM.GetComponent<Player>().Lives >= 1)
         {
             EnemyRandomX = Random.Range(-9.44f, 9.48f);
-            Instantiate(EnemeyPrefab_SM, new Vector3(EnemyRandomX, 6.93f, 0), Quaternion.identity);
+            GameObject NewEnemy = Instantiate(EnemeyPrefab_SM, new Vector3(EnemyRandomX, 6.93f, 0), Quaternion.identity);
+            NewEnemy.transform.parent = EnemyContainer.transform;
             yield return new WaitForSeconds(2.5f);
         }
         if (Player_SM.GetComponent<Player>().Lives <= 0)
