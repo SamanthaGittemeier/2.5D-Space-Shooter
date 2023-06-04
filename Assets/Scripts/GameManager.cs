a9e693971363ee955f238cf57fcee2773ea477e6
Samantha Gittemeier <sammig249@gmail.com>
2023-06-04 15:24:16 -0500

    Working on Scaling UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,6 +27,10 @@ public class GameManager : MonoBehaviour
     private float _currentClipTime;
     [SerializeField]
     private float _currentClipLength;
+    [SerializeField]
+    private float _canSprint = -1f; //can fire
+    [SerializeField]
+    private float _sprintTime; //fire rate or delay
 
     // Start is called before the first frame update
     void Start()
@@ -53,26 +57,72 @@ public class GameManager : MonoBehaviour
         _currentClipInfo = _fuelBarAnim.GetCurrentAnimatorClipInfo(0);
         _currentClipName = _currentClipInfo[0].clip.name;
         _currentClipLength = _currentClipInfo[0].clip.length;
-        _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
+        //_currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
+
+        if (_currentClipTime == 10)
+        {
+            _playerGM.ResetSpeed();
+            _fuelBarAnim.SetBool("ShiftDown", false);
+        }
 
         if (Input.GetKey(KeyCode.LeftShift) && _currentClipName == "FuelBarFull_A" || Input.GetKey(KeyCode.RightShift))
         {
+            //_currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
             _playerGM.Sprint();
             _fuelBarAnim.SetBool("ShiftDown", true);
-            if (_currentClipTime >= 10)
-            {
-                _playerGM.ResetSpeed();
-                _fuelBarAnim.SetBool("ShiftDown", false);
-                _fuelBarAnim.Play("FuelBarRefilling");
-            }
+            //if (_fuelBarAnim.GetBool("ShiftDown") == true)
+            //{
+            //    _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
+            //    if (_currentClipTime == 10)
+            //    {
+            //        _playerGM.ResetSpeed();
+            //        _fuelBarAnim.SetBool("ShiftDown", false);
+            //    }
+            //}
+            //while (_fuelBarAnim.GetBool("ShiftDown") == true)
+            //{
+            //    _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
+            //    if (_currentClipTime >= 10)
+            //    {
+            //        _playerGM.ResetSpeed();
+            //        _fuelBarAnim.SetBool("ShiftDown", false);
+            //    }
+            //}
+            //StartCoroutine(OutOfFuel());
+            //_sprintTime = _currentClipLength;
+            //_canSprint = Time.time + _sprintTime;
+            //if (Time.time < _canSprint)
+            //{
+            //    _playerGM.ResetSpeed();
+            //    _fuelBarAnim.SetBool("ShiftDown", false);
+            //    //_fuelBarAnim.Play("FuelBarRefilling");
+            //}
+            //if (_currentClipTime >= 10f)
+            //{
+            //    _playerGM.ResetSpeed();
+            //    _fuelBarAnim.SetBool("ShiftDown", false);
+            //}
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift) && _currentClipName == "FuelBarDepleting_A" || Input.GetKeyUp(KeyCode.RightShift))
         {
             _playerGM.ResetSpeed();
             _fuelBarAnim.SetBool("ShiftDown", false);
-            _fuelBarAnim.Play("FuelBarRefilling");
+            //_fuelBarAnim.Play("FuelBarRefilling");
         }
     }
+
+    //private IEnumerator OutOfFuel()
+    //{
+    //    while (_fuelBarAnim.GetBool("ShiftDown") == true)
+    //    {
+    //        _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
+    //        if (_currentClipTime >= 10)
+    //        {
+    //            _playerGM.ResetSpeed();
+    //            _fuelBarAnim.SetBool("ShiftDown", false);
+    //        }
+    //    }
+    //}
 
     public void GameOver()
     {
