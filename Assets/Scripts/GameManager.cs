5a46a50692c1477a8a4587c1c52b916a3aafeac9
Samantha Gittemeier <sammig249@gmail.com>
2023-06-04 17:35:48 -0500

    Thruster Scaling UI Implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,12 +25,6 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private float _currentClipTime;
-    [SerializeField]
-    private float _currentClipLength;
-    [SerializeField]
-    private float _canSprint = -1f; //can fire
-    [SerializeField]
-    private float _sprintTime; //fire rate or delay
 
     // Start is called before the first frame update
     void Start()
@@ -56,73 +50,29 @@ public class GameManager : MonoBehaviour
         _currentAnimStateInfo = _fuelBarAnim.GetCurrentAnimatorStateInfo(0);
         _currentClipInfo = _fuelBarAnim.GetCurrentAnimatorClipInfo(0);
         _currentClipName = _currentClipInfo[0].clip.name;
-        _currentClipLength = _currentClipInfo[0].clip.length;
-        //_currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
 
-        if (_currentClipTime == 10)
+        if (_fuelBarAnim.GetBool("ShiftDown") == true)
         {
-            _playerGM.ResetSpeed();
-            _fuelBarAnim.SetBool("ShiftDown", false);
+            _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
+            if (_currentClipTime >= 10)
+            {
+                _playerGM.ResetSpeed();
+                _fuelBarAnim.SetBool("ShiftDown", false);
+                _fuelBarAnim.Play("FuelBarRefilling_A");
+            }
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) && _currentClipName == "FuelBarFull_A" || Input.GetKey(KeyCode.RightShift))
+        if (Input.GetKey(KeyCode.LeftShift) && _currentClipName == "FuelBarFull_A" || Input.GetKey(KeyCode.RightShift) && _currentClipName == "FuelBarFull_A")
         {
-            //_currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
             _playerGM.Sprint();
             _fuelBarAnim.SetBool("ShiftDown", true);
-            //if (_fuelBarAnim.GetBool("ShiftDown") == true)
-            //{
-            //    _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
-            //    if (_currentClipTime == 10)
-            //    {
-            //        _playerGM.ResetSpeed();
-            //        _fuelBarAnim.SetBool("ShiftDown", false);
-            //    }
-            //}
-            //while (_fuelBarAnim.GetBool("ShiftDown") == true)
-            //{
-            //    _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
-            //    if (_currentClipTime >= 10)
-            //    {
-            //        _playerGM.ResetSpeed();
-            //        _fuelBarAnim.SetBool("ShiftDown", false);
-            //    }
-            //}
-            //StartCoroutine(OutOfFuel());
-            //_sprintTime = _currentClipLength;
-            //_canSprint = Time.time + _sprintTime;
-            //if (Time.time < _canSprint)
-            //{
-            //    _playerGM.ResetSpeed();
-            //    _fuelBarAnim.SetBool("ShiftDown", false);
-            //    //_fuelBarAnim.Play("FuelBarRefilling");
-            //}
-            //if (_currentClipTime >= 10f)
-            //{
-            //    _playerGM.ResetSpeed();
-            //    _fuelBarAnim.SetBool("ShiftDown", false);
-            //}
         }
-        else if (Input.GetKeyUp(KeyCode.LeftShift) && _currentClipName == "FuelBarDepleting_A" || Input.GetKeyUp(KeyCode.RightShift))
+        else if (Input.GetKeyUp(KeyCode.LeftShift) && _currentClipName == "FuelBarDepleting_A" || Input.GetKeyUp(KeyCode.RightShift) && _currentClipName == "FuelBarDepleting_A")
         {
             _playerGM.ResetSpeed();
             _fuelBarAnim.SetBool("ShiftDown", false);
-            //_fuelBarAnim.Play("FuelBarRefilling");
         }
     }
-
-    //private IEnumerator OutOfFuel()
-    //{
-    //    while (_fuelBarAnim.GetBool("ShiftDown") == true)
-    //    {
-    //        _currentClipTime = _currentClipInfo[0].clip.length * _currentAnimStateInfo.normalizedTime;
-    //        if (_currentClipTime >= 10)
-    //        {
-    //            _playerGM.ResetSpeed();
-    //            _fuelBarAnim.SetBool("ShiftDown", false);
-    //        }
-    //    }
-    //}
 
     public void GameOver()
     {
